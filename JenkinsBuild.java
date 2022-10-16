219ad3391e45621859e92eafad9853cfbe0c8275
TemateuRoslyn <65954146+TemateuRoslyn@users.noreply.github.com>
2022-10-16 18:18:40 +0100

    ajout du sop d'update

--- JenkinsBuild.java
+++ JenkinsBuild.java
@@ -1,6 +1,7 @@
 
 public class JenkinsBuild {
   public static void main (String [] args) {
+    System.out.println("Update");
     System.out.println("From Github");
     System.out.println("Hello Bg");
     System.out.println("Hello you're well !!");

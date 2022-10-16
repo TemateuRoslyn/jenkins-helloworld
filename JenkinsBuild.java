c2022e86f32aa20c89d1e39eeeebe2d88495e2fb
TemateuRoslyn <65954146+TemateuRoslyn@users.noreply.github.com>
2022-10-16 14:36:53 +0100

    Update JenkinsBuild.java

--- JenkinsBuild.java
+++ JenkinsBuild.java
@@ -1,1 +1,8 @@
 
+public class JenkinsBuild {
+  public static void main (String [] args) {
+    System.out.println("From Github");
+    System.out.println("Hello Bg");
+    System.out.println("Hello you're well !!");
+  }
+}
